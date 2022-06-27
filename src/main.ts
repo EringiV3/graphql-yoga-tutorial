06187ac14bff9d6a634399554b5eb5950f64e952
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2022-06-27 13:34:19 +0900

    pothos setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createServer } from '@graphql-yoga/node';
-import { createContext } from './context';
 import { schema } from './schema';
 
 async function main() {
-  const server = createServer({ schema, context: createContext });
+  const server = createServer({ schema });
   await server.start();
 }
 
--- src/schema.ts
+++ src/schema.ts
@@ -1,108 +1,41 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { GraphQLYogaError } from '@graphql-yoga/node';
-import type { Link } from '@prisma/client';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import type { GraphQLContext } from './context';
+import { builder } from './builder';
+import { db } from './db';
 
-const typeDefinitions = /* GraphQL */ `
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-    comments: [Comment!]!
-  }
-
-  type Comment {
-    id: ID!
-    body: String!
-  }
-
-  type Query {
-    info: String!
-    feed: [Link!]!
-    comment(id: ID!): Comment
-  }
-
-  type Mutation {
-    postLink(url: String!, description: String!): Link!
-    postCommentOnLink(linkId: ID!, body: String!): Comment!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent: unknown, args: {}, context: GraphQLContext) => {
-      return context.prisma.link.findMany();
-    },
-    comment: async (
-      parent: unknown,
-      args: { id: string },
-      context: GraphQLContext
-    ) => {
-      return context.prisma.comment.findUnique({
-        where: { id: parseInt(args.id) },
-      });
-    },
-  },
-  Link: {
-    id: (parent: Link) => parent.id,
-    description: (parent: Link) => parent.description,
-    url: (parent: Link) => parent.url,
-    comments: (parent: Link, args: {}, context: GraphQLContext) => {
-      return context.prisma.comment.findMany({
-        where: {
-          linkId: parent.id,
-        },
-      });
-    },
-  },
-  Mutation: {
-    postLink: async (
-      parent: unknown,
-      args: { description: string; url: string },
-      context: GraphQLContext
-    ) => {
-      const newLink = await context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-      return newLink;
-    },
-    postCommentOnLink: async (
-      parent: unknown,
-      args: { linkId: string; body: string },
-      context: GraphQLContext
-    ) => {
-      const comment = await context.prisma.comment
-        .create({
-          data: {
-            body: args.body,
-            linkId: parseInt(args.linkId),
-          },
-        })
-        .catch((err: unknown) => {
-          if (
-            err instanceof PrismaClientKnownRequestError &&
-            err.code === 'P2003'
-          ) {
-            return Promise.reject(
-              new GraphQLYogaError(
-                `Cannot post comment on non-existing link with id '${args.linkId}'.`
-              )
-            );
-          }
-          return Promise.reject(err);
-        });
-
-      return comment;
-    },
-  },
-};
-
-export const schema = makeExecutableSchema({
-  resolvers: [resolvers],
-  typeDefs: [typeDefinitions],
+builder.prismaObject('Link', {
+  fields: (t) => ({
+    id: t.exposeID('id'),
+    description: t.exposeString('description'),
+    url: t.exposeString('url'),
+    comments: t.relation('comments'),
+  }),
 });
+
+builder.prismaObject('Comment', {
+  fields: (t) => ({
+    id: t.exposeID('id'),
+    body: t.exposeString('body'),
+    link: t.relation('link'),
+  }),
+});
+
+builder.queryType({
+  fields: (t) => ({
+    link: t.prismaField({
+      type: 'Link',
+      nullable: true,
+      args: {
+        id: t.arg.id({ required: true }),
+      },
+      resolve: (query, root, args) => {
+        return db.link.findUnique({
+          ...query,
+          where: {
+            id: Number.parseInt(String(args.id), 10),
+          },
+        });
+      },
+    }),
+  }),
+});
+
+export const schema = builder.toSchema({});
